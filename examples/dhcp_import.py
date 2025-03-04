8671f71c5b521fcf6751f079529b6b5ecea55908
KHT-Admin <171313330+KHT-Admin@users.noreply.github.com>
2025-03-04 12:25:32 +1100

    Add argparser

--- examples/dhcp_import.py
+++ examples/dhcp_import.py
@@ -1,3 +1,4 @@
+import argparse
 import re
 
 import pyunifi_ng.client as pyunifi_ng
@@ -14,22 +15,13 @@ def read_file(path: str) -> str:
 
 
 def extract_static(data: str, pattern):
+    decoder = OPNS if pattern == "OPNS" else ER
+
     return re.finditer(
-        pattern,
+        decoder,
         data,
         re.I,
     )
-
-
-def device_upload(data: list[dict]) -> dict:
-    failed = []
-    for record in data:
-        try:
-            c.add_dhcp_reservation(record)
-        except requests.exceptions.HTTPError:
-            """API returns error if subnet is not configured or reservation exists"""
-            failed.append(record)
-    return {"n_failed": len(failed), "failures": failed}
 
 
 def device_encode(data: iter) -> list[dict]:
@@ -39,8 +31,6 @@ def device_encode(data: iter) -> list[dict]:
 
         if "fixed_ip" in device:
             device.update({"use_fixedip": True})
-        else:
-            device.update({"use_fixedip": False})
 
         if "local_dns_record" in device:
             device.update({"local_dns_record_enabled": True})
@@ -49,16 +39,44 @@ def device_encode(data: iter) -> list[dict]:
     return reservations
 
 
-def process(path: str, pattern):
+def device_upload(data: list[dict], c) -> dict:
+    failed = []
+    c.login()
+    for record in data:
+        try:
+            c.add_dhcp_reservation(record)
+        except requests.exceptions.HTTPError:
+            """API returns error if subnet is not configured or reservation exists"""
+            failed.append(record)
+    return {"n_failed": len(failed), "failed": failed}
+
+
+def process(path: str, pattern, client):
     s = extract_static(read_file(path), pattern=pattern)
     d = device_encode(s)
-    return device_upload(d)
+
+    return device_upload(d, client)
 
 
-path = "../tests/opnsense.xml"
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path")
+    parser.add_argument("decoder", choices=["ER", "OPNS"], help="config type")
+    parser.add_argument("user", help="username")
+    parser.add_argument("pwd", help="password")
+    parser.add_argument("host")
+    parser.add_argument("--port", type=int, choices=[443, 8443])
 
-c = pyunifi_ng.Client("admin", "admin", host="127.0.0.1", port=8443)
-c.login()
+    return parser.parse_args()
 
-err = process(path, OPNS)
-print(err)
+
+def main():
+    a = parse_args()
+    c = pyunifi_ng.Client(a.user, a.pwd, host=a.host, port=a.port)
+
+    r = process(a.path, a.decoder, c)
+    print(r)
+
+
+if __name__ == "__main__":
+    main()
