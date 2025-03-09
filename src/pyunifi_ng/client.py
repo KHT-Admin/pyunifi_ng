5714755a421b6f69293c3b1049de4f08254fafc2
KHT-Admin <171313330+KHT-Admin@users.noreply.github.com>
2025-03-09 17:19:55 +1100

    Various fixes

--- src/pyunifi_ng/client.py
+++ src/pyunifi_ng/client.py
@@ -44,7 +44,10 @@ class Client:
         self.site_id = site_id
 
     def logout(self):
-        self._api_write("logout")
+        self._request(
+            "post",
+            f"{self._base_url}{self.auth_path}/logout",
+        )
         self.session.close()
 
     def login(self):
@@ -71,12 +74,18 @@ class Client:
 
         return obj.get("data", obj)
 
-    def _request(self, fn, url, params):
-        return getattr(self.session, fn)(
-            url,
-            **params,
-            headers=self.headers,
-        )
+    def _request(self, fn, url, params=None):
+        if params is None:
+            return getattr(self.session, fn)(
+                url,
+                headers=self.headers,
+            )
+        else:
+            return getattr(self.session, fn)(
+                url,
+                **params,
+                headers=self.headers,
+            )
 
     def _update_tokens(self, header: dict) -> None:
         if "X-CSRF-Token" in header:
@@ -107,7 +116,7 @@ class Client:
         try:
             response = self._request(
                 "post",
-                f"{self._base_url}{self.login_path}",
+                f"{self._base_url}{self.auth_path}/login",
                 {
                     "json": {
                         "username": self.username,
@@ -125,19 +134,24 @@ class Client:
             raise APIError("Login failed - status code: %i" % response.status_code)
 
     def _api_base(
-        self, method, url, api: API, params=None, site_id: Optional[str] = None
+        self,
+        method,
+        url,
+        api: API,
+        params: Optional[dict] = None,
+        site_id: Optional[str] = None,
     ):
         return self._request(
             method,
             f"{self._api_url(api, site_id)}{url}",
-            {"json": params},
+            params,
         )
 
     def _api_read(
         self,
         url,
         api: API,
-        params=None,
+        params: Optional[dict] = None,
         site_id: Optional[str] = None,
     ):
         return self._response_process(self._api_base("get", url, api, params, site_id))
@@ -158,7 +172,7 @@ class Client:
         params: Optional[str] = None,
         site_id: Optional[str] = None,
     ):
-        return self._response_process(self._api_base("put", url, api, params, site_id))
+        return self._response_process(self._api_base("put", url, api, params))
 
     def _api_delete(
         self,
@@ -181,21 +195,29 @@ class Client:
 
         if p == "network":
             self.is_unifi_os = True
-            self.login_path = "api/auth/login"
+            self.auth_path = "api/auth"
         else:
-            self.login_path = "api/login"
+            self.auth_path = "api"
 
-    def get_sites_overview(self, search_text=""):
+    def get_sites_overview(
+        self,
+        pageSize: int = 1000,
+        pageNumber: int = 0,
+        searchText: str = "",
+        _id: str = "",
+    ):
         return self._api_write(
             "sites/overview",
             params={
-                "searchText": search_text,
-                "pageSize": 10000,
-                "pageNumber": 0,
+                "json": {
+                    "pageSize": pageSize,
+                    "pageNumber": pageNumber,
+                    "searchText": searchText,
+                },
             },
             api=API.v2b,
-            site_id="",
-        ).text
+            site_id=_id,
+        ).json()
 
     def get_site_ids(self, search_text=""):
         r = self.get_sites_overview(search_text)
@@ -205,6 +227,27 @@ class Client:
         Return a list of all active UniFi devices
         """
         return self._api_read("device", api=API.v2)
+
+    def get_clients(self):
+        return self._api_read("clients/active", api=API.v2)
+
+    def get_client_history(
+        self,
+        onlyNonBlocked=True,
+        includeUnifiDevices=True,
+        withinHours=0,
+    ):
+        return self._api_read(
+            "clients/history",
+            params={
+                "params": {
+                    "onlyNonBlocked": onlyNonBlocked,
+                    "includeUnifiDevices": includeUnifiDevices,
+                    "withinHours": withinHours,
+                }
+            },
+            api=API.v2,
+        )
 
     def get_firewall_policies(
         self,
@@ -225,9 +268,17 @@ class Client:
 
         return self._api_update(
             f"firewall-policies/{id}" if id else "firewall-policies",
-            params=policy,
+            params={
+                "json": policy,
+            },
             api=API.v2,
         )
 
     def add_dhcp_reservation(self, data: dict):
-        self._api_write("rest/user", params=data, api=API.v1)
+        self._api_write(
+            "rest/user",
+            params={
+                "json": data,
+            },
+            api=API.v1,
+        )
